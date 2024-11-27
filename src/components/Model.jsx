971b1dbc915d678a3240d2270a4ebf595ff3ee05
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-11-27 12:37:43 +0530

    Added Color Palette and Sizes for iPhone Model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,6 +7,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -16,7 +17,7 @@ const Model = () => {
     img: yellowImg,
   });
 
-  // camera controo for the modl view
+  // camera control for the modl view
   const cameraControlSmall = useRef();
   const cameraControlLarge = useRef();
 
@@ -74,6 +75,36 @@ const Model = () => {
             >
               <View.Port />
             </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModel(item)}
+                  />
+                ))}
+              </ul>
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
+            </div>
           </div>
         </div>
       </div>
